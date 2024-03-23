2f0a62e929e31016e6c0234d77536dcda0712f28
Gisele <belagisa14@gmail.com>
2024-03-23 01:07:59 -0300

    first commit

---
+++ src/pages/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from '../../styles/Footer.module.css'
+
+export default function Footer() {
+    return (
+      <footer className={styles.footer}>
+        <p>
+            <span>PokeLand</span> &copy; 2024
+        </p>
+      </footer>
+    );
+  }
+  
---
+++ src/pages/components/Navbar.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import styles from '../../styles/Navbar.module.css'
+
+export default function Navbar() {
+    return (
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+            <Image 
+              src="/images/Pokebola.png" 
+              width="30" 
+              height="30" 
+              alt="PokeLand"
+            />
+            <h1>PokeLand</h1>
+        </div>
+        <ul className={styles.link_items}>
+            <li>
+                <Link href="/" legacyBehavior><a id="link">Home</a></Link>
+            </li>
+            <li>
+                <Link href="/about" legacyBehavior><a id="link">Sobre</a></Link>
+            </li>
+        </ul>
+      </nav>
+    );
+  }
+  
