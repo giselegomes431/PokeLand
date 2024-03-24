63d866aaca095d4839786061c2c54ca02281ab83
Gisele <belagisa14@gmail.com>
2024-03-24 19:12:40 -0300

    feat:page about

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+import styles from '../styles/About.module.css'
+
+export default function About() {
+    return (
+        <div className={styles.about}>
+            <h1>Sobre o projeto</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In repellat impedit neque et sapiente ipsam laboriosam adipisci illum. lure voluptate itaque culpa minus ipsa architecto doloruml Dignissimos recusandae ea aperiam?</p>
+            <Image  src="/images/about.png" width="300" height="300" alt="About"/>
+        </div>
+    );
+  }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
 import Image from "next/image";
+import styles from "../styles/Home.module.css"
 
+export async function getStaticProps() {
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
 
-export default function Home() {
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+  console.log(data)
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+}
+
+export default function Home({pokemons}) {
   return (
-    <h1>PokeLand</h1>
+    <div className={styles.home}>
+      <h1>PokeLand</h1>
+      <ul>
+        {pokemons?.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
