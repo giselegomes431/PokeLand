ba9742536c9d91d5a0807f083748b6816234aaa9
Gisele <belagisa14@gmail.com>
2024-03-27 00:22:23 -0300

    style: about

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,8 +6,14 @@ export default function About() {
     return (
         <div className={styles.about}>
             <h1>Sobre o projeto</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In repellat impedit neque et sapiente ipsam laboriosam adipisci illum. lure voluptate itaque culpa minus ipsa architecto doloruml Dignissimos recusandae ea aperiam?</p>
-            <Image  src="https://img.pokemondb.net/sprites/home/normal/jigglypuff.png" width="300" height="300" alt="About"/>
+            <p>Este é um projeto pessoal criado com o propósito de estudar a linguagem Next.js.</p>
+            <Image  
+            src="https://img.pokemondb.net/sprites/home/normal/pidgeot.png" 
+            width="300" 
+            height="300" 
+            alt="About"
+            className={styles.image}
+            />
         </div>
     );
   }
