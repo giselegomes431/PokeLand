5e64fe5322e2dfbab53f27443c34a8dc66c0f34c
Gisele <belagisa14@gmail.com>
2024-03-26 22:12:09 -0300

    feat: pokemonId

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css"
-import Card from './components/Card'
+import Card from '../../components/Card'
 
 export async function getStaticProps() {
   const maxPokemons = 251
@@ -8,7 +8,6 @@ export async function getStaticProps() {
 
   const res = await fetch(`${api}/?limit=${maxPokemons}`)
   const data = await res.json()
-  console.log(data)
 
   data.results.forEach((item, index) => {
     item.id = index + 1
